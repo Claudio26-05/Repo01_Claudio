71992f9ea573fb641cc9821b4a6f239c5dfc02bd
Claudio <Claudiosthephano2003@gmail.com>
2025-08-04 10:59:54 -0500

    Tercer commit creando archivo02

---
+++ archivo02.py
@@ -0,0 +1,14 @@
+#Generando una Calculadora basica en python
+
+num01=int(input("Ingresa un numero bro:\n"))
+num02=int(input("Ingresa otro numero bro:\n"))
+
+def resultado(a,b):
+    return a+b
+
+print (f"El resultado de la suma de {num01} y {num02} es : {resultado(num01,num02)}")
+
+
+
+
+
