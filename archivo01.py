6929342bb98e826fc3102d9f3446b4724db9bbf7
Claudio <Claudiosthephano2003@gmail.com>
2025-08-04 11:15:06 -0500

    Cuarto Commit- Modificación de ambos archivos

--- archivo01.py
+++ archivo01.py
@@ -1,8 +1,12 @@
 nombre=input("Como te llamas?\n")
+edad=int(input("Ingresa tu edad :\n"))
 def Saludo(name):
     print(f"Hola como te va {name}")
 
+def Edad_B(edad):
+    print(f"Tu edad es :{edad}")
 Saludo(nombre)
+Edad_B(edad)
 
 
 
--- archivo02.py
+++ archivo02.py
@@ -3,12 +3,20 @@
 num01=int(input("Ingresa un numero bro:\n"))
 num02=int(input("Ingresa otro numero bro:\n"))
 
-def resultado(a,b):
-    return a+b
+op= input("Ingresa el operador:\n1.+\n2.-\n3./\n4.*\n")
+def resultado(a,b,c):
+    if c in ["+", "-", "/", "*"]:
+        if c=="+":
+            return a+b
+        elif c=="-":
+            return a-b
+        elif c=="/":
+            return a/b
+        elif c=="*":
+            return a*b
+    else:
+        print("Elige los signos, no los números!!!")
 
-print (f"El resultado de la suma de {num01} y {num02} es : {resultado(num01,num02)}")
-
-
-
+print (f"El resultado de la suma de {num01} y {num02} es : {resultado(num01,num02,op)}")
 
 
